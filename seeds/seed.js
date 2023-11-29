d25647a3ea50b3b5b350a661abcc924d32885087
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-11-28 20:13:31 -0600

    Pushing to Heroku

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,5 +3,15 @@ const { User } = require('../models');
 
 const userData = require('./userData.json');
 
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
 
 seedDatabase();
