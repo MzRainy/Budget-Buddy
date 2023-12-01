abbfcfae3fd121845ec8a9efd3a2878d6651e7ef
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-11-30 18:30:45 -0600

    added eslint files and package to dependencies. changed database name

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-DROP DATABASE IF EXISTS user_db;
-CREATE DATABASE user_db;
+DROP DATABASE IF EXISTS budgetBuddy_db;
 
-DROP DATABASE IF EXISTS categories_db;
-CREATE DATABASE categories_db;
+CREATE DATABASE budgetBuddy_db;
