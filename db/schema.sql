a1e6f4936585208eb21727b43069da091220c88b
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-11-30 20:31:32 -0600

    removing sql creds from schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-DB_NAME='budgetBuddy_db'
-DB_USER='root'
-DB_PASSWORD='StarStream27#'
-=======
 DROP DATABASE IF EXISTS budgetBuddy_db;
 
 CREATE DATABASE budgetBuddy_db;
->>>>>>> main
