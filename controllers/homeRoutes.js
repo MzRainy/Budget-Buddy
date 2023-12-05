91f0e28a4992d5a47b9d002c0bd99eae044d86e6
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-12-04 20:47:37 -0600

    corrected route to homepage. Some styling for nav bar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,33 +3,42 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // TODO: Add a comment describing the functionality of the withAuth middleware
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // TODO: Add a comment describing the functionality of this property
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
+router.get('/', async (req, res) => {
+  try { 
+    res.render('homepage');
+  } catch(err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  // TODO: Add a comment describing the functionality of this if statement
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
 
-  res.render('login');
-});
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render('homepage', {
+//       users,
+//       // TODO: Add a comment describing the functionality of this property
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/login', (req, res) => {
+//   // TODO: Add a comment describing the functionality of this if statement
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('login');
+// });
 
 module.exports = router;
