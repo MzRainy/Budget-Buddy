a59c3acf6c2517160d21c9be4ea36a7b6b573942
Cristian <139021004+Cbaca4@users.noreply.github.com>
2023-12-05 21:29:01 -0600

    login js

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,35 @@
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-  document.addEventListener('DOMContentLoaded', function() {
-    const contentDiv = document.getElementById('content');
-    const loginSource = document.getElementById('login-template').innerHTML;
-    const userSource = document.getElementById('user-template').innerHTML;
+document.querySelector('.login-form');
+document.addEventListener('submit', loginFormHandler);
 
-    const loginTemplate = Handlebars.compile(loginSource);
-    const userTemplate = Handlebars.compile(userSource);
+document.addEventListener('DOMContentLoaded', function() {
+  const contentDiv = document.getElementById('content');
+  const loginSource = document.getElementById('login-template').innerHTML;
+  const userSource = document.getElementById('user-template').innerHTML;
+  const loginTemplate = Handlebars.compile(loginSource);
+  const userTemplate = Handlebars.compile(userSource);
 
-    function renderLoginForm() {
-      contentDiv.innerHTML = loginTemplate();
-      document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+  function renderLoginForm() {
+    contentDiv.innerHTML = loginTemplate();
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
 
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
 
-        if (username === 'user' && password === 'password') {
-          const userData = {
-            username: username,
-            email: 'example@example.com'
-          };
-          renderUserInfo(userData);
-        } else {
-          contentDiv.innerHTML += '<p>Invalid credentials. Please try again.<p>'
-        }
-      });
-    }
-    function renderUserInfo(userData) {
-      contentDiv.innerHTML = userTemplate(userData);
-    }
-    renderLoginForm();
-  });
+      if (username === 'user' && password === 'password') {
+        const userData = {
+          username: username,
+          email: 'example@example.com'
+        };
+        renderUserInfo(userData);
+      } else {
+        contentDiv.innerHTML += 'Invalid credentials. Please try again.';
+      }
+    });
+  }
+  function renderUserInfo(userData) {
+    contentDiv.innerHTML = userTemplate(userData);
+  }
+  renderLoginForm();
+});
