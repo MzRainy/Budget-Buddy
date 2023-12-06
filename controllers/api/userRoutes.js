8eb537d66d0bdd96f0f5e1c5889cfa589cfd332e
DamascusKraken <nick.canchola.7.27@gmail.com>
2023-12-06 07:04:00 -0600

    Login Page: Redone handlebars and js file. Fixed session updating for navBar

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,8 +3,7 @@ const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
-    // TODO: Add a comment describing the functionality of this expression
-    const userData = await User.findOne({ where: { email: req.body.email } });
+  
 
     if (!userData) {
       res
@@ -13,7 +12,6 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    // TODO: Add a comment describing the functionality of this expression
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
@@ -23,7 +21,7 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    // TODO: Add a comment describing the functionality of this method
+    
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -38,7 +36,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
-    // TODO: Add a comment describing the functionality of this method
+    
     req.session.destroy(() => {
       res.status(204).end();
     });
